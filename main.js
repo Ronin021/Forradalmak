69b40a1e5b8ab38da4dfa5458cc1eec37db2c6b2
Ronin021 <lavro.sandor@gmail.com>
2025-04-07 15:23:12 +0200

    tablazat hozzaadas

--- main.js
+++ main.js
@@ -16,6 +16,43 @@ const divtable = makeDiv('table')
 
 // A divtable-t hozzáadjuk a containeren belülre
 divcontainer.appendChild(divtable)
+// Létrehozunk egy <table> elemet
+const table = document.createElement('table')
+
+// A táblázatot hozzáadjuk a korábban létrehozott 'divtable' elemhez
+divtable.appendChild(table)
+
+// Létrehozunk egy <thead> elemet (táblázat fejléce)
+const thead = document.createElement('thead')
+
+// A <thead>-et hozzáadjuk a táblázathoz
+table.appendChild(thead)
+
+// Létrehozunk egy sort a <thead>-hez
+const theadrow = document.createElement('tr')
+
+// A sort hozzáadjuk a <thead>-hez
+thead.appendChild(theadrow)
+
+// Oszlopnevek listája
+const cella = ['forradalom', 'evszam', 'sikeres']
+
+// Végigmegyünk az oszlopneveken, és létrehozunk hozzájuk egy-egy <th> cellát
+for (const cellacontent of cella) {
+    const theadcella = document.createElement('th') // Új fejléc cella
+    theadcella.innerText = cellacontent // Beállítjuk a cella szövegét
+
+    theadrow.appendChild(theadcella) // Hozzáadjuk a cellát a sorhoz
+}
+
+// Létrehozunk egy <tbody> elemet (táblázat törzse)
+const tbody = document.createElement('tbody')
+
+// A <tbody>-t hozzáadjuk a táblázathoz
+table.appendChild(tbody)
+
+
+
 
 // Létrehozunk egy 'form' class nevű divet
 const divform = makeDiv('form')
