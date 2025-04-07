6db1949981cf35cfd6529387236dfe8ea9dd9716
Ronin021 <lavro.sandor@gmail.com>
2025-04-07 15:06:25 +0200

    div struktura-comment

--- main.js
+++ main.js
@@ -1,18 +1,24 @@
-const makeDiv = (className)=> {
-    const div = document.createElement('div')
-
-    div.className
-    return div
+// Egy segédfüggvény, ami létrehoz egy <div> elemet a megadott class névvel
+const makeDiv = (className) => {
+    const div = document.createElement('div') // Létrehozunk egy új <div> elemet
+    div.className = className // Beállítjuk a class nevét
+    return div // Visszaadjuk az elkészített <div>-et
 }
 
+// Létrehozunk egy 'container' class nevű divet
 const divcontainer = makeDiv('container')
 
+// Hozzáadjuk a divcontainer-t a dokumentum body-jához
 document.body.appendChild(divcontainer)
 
+// Létrehozunk egy 'table' class nevű divet
 const divtable = makeDiv('table')
 
+// A divtable-t hozzáadjuk a containeren belülre
 divcontainer.appendChild(divtable)
 
-const divform = document.makeDiv('form')
+// Létrehozunk egy 'form' class nevű divet
+const divform = makeDiv('form')
 
+// A divform-ot is hozzáadjuk a containerhez
 divcontainer.appendChild(divform)
